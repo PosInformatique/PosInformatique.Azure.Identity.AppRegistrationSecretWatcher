df1c1a5264691bcf59bf5cd6e00ab074b8c6a7da
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2025-11-10 16:16:42 +0100

    Implementation of the Azure Function.

---
+++ src/Functions/AppRegistrationSecretWatcherFunctionsOptions.cs
@@ -0,0 +1,20 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AppRegistrationSecretWatcherFunctionsOptions.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.Azure.Identity.AppRegistrationSecretWatcher.Functions
+{
+    public class AppRegistrationSecretWatcherFunctionsOptions
+    {
+        public AppRegistrationSecretWatcherFunctionsOptions()
+        {
+            this.TenantIds = new Collection<string>();
+        }
+
+        public Collection<string> TenantIds { get; }
+
+        public TimeSpan ExpirationThreshold { get; set; }
+    }
+}
---
+++ tests/Functions.Tests/AppRegistrationSecretWatcherFunctionsOptionsTest.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AppRegistrationSecretWatcherFunctionsOptionsTest.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.Azure.Identity.AppRegistrationSecretWatcher.Functions.Tests
+{
+    public class AppRegistrationSecretWatcherFunctionsOptionsTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            var options = new AppRegistrationSecretWatcherFunctionsOptions();
+
+            options.ExpirationThreshold.Should().Be(TimeSpan.Zero);
+            options.TenantIds.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void ExpirationThreshold_ValueChanged()
+        {
+            var options = new AppRegistrationSecretWatcherFunctionsOptions();
+
+            options.ExpirationThreshold = TimeSpan.FromDays(4);
+
+            options.ExpirationThreshold.Should().Be(TimeSpan.FromDays(4));
+        }
+    }
+}
