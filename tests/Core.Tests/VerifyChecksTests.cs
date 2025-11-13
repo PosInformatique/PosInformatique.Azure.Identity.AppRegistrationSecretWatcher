db885446a39827c0fe911b900eed5afe9ca6bb66
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2025-11-13 12:31:26 +0100

    Add unit tests to fix Verify() setup.

---
+++ tests/Core.Tests/VerifyChecksTests.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="VerifyChecksTests.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.Azure.Identity.AppRegistrationSecretWatcher.Core.Tests
+{
+    public class VerifyChecksTests
+    {
+        [Fact]
+        public Task Run() => VerifyChecks.Run();
+    }
+}
