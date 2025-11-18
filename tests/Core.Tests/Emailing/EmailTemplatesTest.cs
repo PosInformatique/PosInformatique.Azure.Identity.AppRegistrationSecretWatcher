e9b2123ba073ccee3838fc9318884d59ec262636
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2025-11-18 16:19:06 +0100

    Add a summary indicators in the report template.

---
+++ tests/Core.Tests/Emailing/EmailTemplatesTest.cs
@@ -0,0 +1,86 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EmailTemplatesTest.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.Azure.Identity.AppRegistrationSecretWatcher.Emailing.Tests
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class EmailTemplatesTest
+    {
+        [Fact]
+        public async Task ReportEmailTemplateBody_Render()
+        {
+            var checkResult = new AppRegistrationSecretCheckResult(
+            [
+                new AppRegistrationSecretCheckResultTenant(
+                    "Id 1",
+                    "The tenant 1",
+                    [
+                        new AppRegistrationSecretCheckResultApplication(
+                            "Id 1-1",
+                            "The app 1-1",
+                            [
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 1-1-1", new DateTime(2025, 1, 1), -10) { Status = AppRegistrationSecretStatus.Expired },
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 1-1-2", new DateTime(2025, 2, 2), 20) { Status = AppRegistrationSecretStatus.Valid },
+                            ]),
+                        new AppRegistrationSecretCheckResultApplication(
+                            "Id 1-2",
+                            "The app 1-2",
+                            [
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 1-2-1", new DateTime(2025, 3, 3), 30) { Status = AppRegistrationSecretStatus.Valid },
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 1-2-2", new DateTime(2025, 4, 4), 40) { Status = AppRegistrationSecretStatus.ExpiringSoon },
+                            ]),
+                        new AppRegistrationSecretCheckResultApplication(
+                            "Id 1-3",
+                            "The app 1-3",
+                            [])
+                    ]),
+                new AppRegistrationSecretCheckResultTenant(
+                    "Id 2",
+                    "The tenant 2",
+                    [
+                        new AppRegistrationSecretCheckResultApplication(
+                            "Id 2-1",
+                            "The app 2-1",
+                            [
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 2-1-1", new DateTime(2025, 5, 5), 50) { Status = AppRegistrationSecretStatus.Valid },
+                            ]),
+                        new AppRegistrationSecretCheckResultApplication(
+                            "Id 2-2",
+                            "The app 2-2",
+                            [
+                                new AppRegistrationSecretCheckResultApplicationSecret("Secret 2-1-1", new DateTime(2025, 6, 6), 60) { Status = AppRegistrationSecretStatus.Valid },
+                            ])
+                    ]),
+                new AppRegistrationSecretCheckResultTenant(
+                    "Id 3",
+                    "The tenant 3",
+                    []),
+            ],
+            new DateTime(2025, 1, 2, 3, 4, 5, 6, DateTimeKind.Utc));
+
+            var serviceCollection = new ServiceCollection();
+
+            var content = await RazorTemplateTools.RenderAsync<ReportEmailTemplateBody>(checkResult, serviceCollection);
+
+            await Verify(content, "html");
+        }
+
+        [Fact]
+        public async Task ReportEmailTemplateSubject_Render()
+        {
+            var checkResult = new AppRegistrationSecretCheckResult(
+                [],
+                new DateTime(2025, 1, 2, 3, 4, 5, 6, DateTimeKind.Utc));
+
+            var serviceCollection = new ServiceCollection();
+
+            var content = await RazorTemplateTools.RenderAsync<ReportEmailTemplateSubject>(checkResult, serviceCollection);
+
+            content.Should().Be("Reminder: App Registration secrets expiring soon - [02/01/2025]");
+        }
+    }
+}
---
+++ tests/Core.Tests/Emailing/RazorTemplateTools.cs
@@ -0,0 +1,70 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RazorTemplateTools.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.JSInterop;
+    using PosInformatique.Foundations.Text.Templating;
+    using PosInformatique.Foundations.Text.Templating.Razor;
+
+    public static class RazorTemplateTools
+    {
+        /*public static void DisplayHtmlPage(string content, string testName)
+        {
+            if (!Debugger.IsAttached)
+            {
+                return;
+            }
+
+            var temporaryFolder = Path.Combine(UnitTestsFolders.TemporaryRoot, "Emailing Template Tests");
+            var temporaryFile = Path.Combine(temporaryFolder, $"{testName}.html");
+
+            if (!Directory.Exists(temporaryFolder))
+            {
+                Directory.CreateDirectory(temporaryFolder);
+            }
+
+            File.WriteAllText(temporaryFile, content);
+
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = temporaryFile,
+                UseShellExecute = true,
+            });
+        }*/
+
+        public static async Task<string> RenderAsync<TComponent>(object model, IServiceCollection services)
+            where TComponent : ComponentBase
+        {
+#pragma warning disable PosInfoMoq1000 // VerifyAll() method should be called when instantiate a Mock<T> instances
+            var jsRuntime = new Mock<IJSRuntime>(MockBehavior.Strict);
+#pragma warning restore PosInfoMoq1000 // VerifyAll() method should be called when instantiate a Mock<T> instances
+
+            services.AddLogging();
+            services.AddSingleton(jsRuntime.Object);
+            services.AddRazorTextTemplating();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var context = new Mock<ITextTemplateRenderContext>(MockBehavior.Strict);
+            context.Setup(c => c.ServiceProvider)
+                .Returns(serviceProvider);
+
+            var template = new RazorTextTemplate<object>(typeof(TComponent));
+
+            var output = new StringWriter();
+
+            await template.RenderAsync(model, output, context.Object);
+
+            context.VerifyAll();
+
+            return output.ToString();
+        }
+    }
+}
